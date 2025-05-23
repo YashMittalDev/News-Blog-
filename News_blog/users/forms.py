536fee671f160d260a24bcd334e8ee0dfcbd3a1d
Yash mittal <mittal.yogesh935145@gmail.com>
2025-05-23 17:38:58 +0530

    loginn logout completed

--- News_blog/users/forms.py
+++ News_blog/users/forms.py
@@ -1,12 +1,48 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+
+class SignUpForm(UserCreationForm):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['username'].label = "Your Username"
+        self.fields['username'].widgets = forms.TextInput()
+        self.fields['email'].label = "Your Email"
+        self.fields['email'].widgets = forms.EmailInput()
+        self.fields['password1'].label = "Your Password"
+        self.fields['password1'].widgets = forms.PasswordInput()
+        self.fields['password2'].label = "Confirm Password"
+        self.fields['password2'].widgets = forms.PasswordInput()
+        
+    class Meta:
+        model = User
+        #fields = '__all__'
+        fields = ['username','email','password1','password2']
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+'''
+class LoginForm(UserCreationForm):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['username'].label = "Your Username"
+        self.fields['username'].widgets = forms.TextInput()
+        self.fields['password1'].label = "Password"
+        self.fields['password1'].widgets = forms.PasswordInput()  
+    class Meta:
+        model = User
+        #fields = '__all__'
+        fields = ['username','password1']
+'''
+
+'''
 class SignUpForm(forms.Form):
     username = forms.CharField()
     email = forms.CharField(widget=forms.EmailInput())
     password = forms.CharField(widget=forms.PasswordInput())
     confirm = forms.CharField(widget=forms.PasswordInput())
-
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput())
+'''
     
--- News_blog/users/views.py
+++ News_blog/users/views.py
@@ -3,13 +3,14 @@ from django.http import HttpResponse,HttpRequest
 from .forms import SignUpForm,LoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
 # Create your views here.
 
 def logIn(request:HttpRequest):
     form = LoginForm()
     if request.method == "POST":
         username = request.POST['username']
-        password = request.POST['password']
+        password = request.POST['password1']
         user = authenticate(request,username=username,password=password)
         if user is not None:
             login(request,user)
@@ -17,7 +18,18 @@ def logIn(request:HttpRequest):
     context = {'form':form}
     return render(request,'users\login.html',context)
 
+
 def sign_up(request:HttpRequest):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"User created succesfully")
+            return redirect('login')
+    context = {'form':form}
+    return render(request,'users\sign_up.html',context) 
+    '''
     form = SignUpForm()
     if request.method == "POST":
         username = request.POST['username']
@@ -31,7 +43,7 @@ def sign_up(request:HttpRequest):
             return redirect('posts_home')
     context = {'form':form}
     return render(request,'users\sign_up.html',context)
-
+    '''
 def log_out(request:HttpRequest):
     logout(request)
     return redirect('posts_home')
